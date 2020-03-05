7104e658e690e577688f8ace35842f62b1c08b46
Dan Padnos <danp@ai21.com>
2020-03-05 19:58:30 +0200

    minor reshuffling in solution

--- sol_autodiff/grad.py
+++ sol_autodiff/grad.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from sol_autodiff.tracing import Node, backward
+from sol_autodiff.tracing import Node
 
 
 def _substitute(tup, idx, value):
@@ -19,3 +19,37 @@ def grad(fun, argnum=0):
         else:
             return backward(np.ones_like(end_node.value), end_node)
     return grad_fun
+
+
+def backward(g, end_node):
+    outgrads = {end_node: g}
+    for node in toposort(end_node):
+        outgrad = outgrads.pop(node)
+        fun, args, kwargs, parents = node.recipe
+        for argnum, parent in parents:
+            vjp = fun.get_vjp(argnum)
+            parent_grad = vjp(outgrad, node.value, *args, **kwargs)
+            outgrads[parent] = outgrads.get(parent, 0) + parent_grad
+    return outgrad
+
+
+def toposort(end_node):
+    child_counts = {}
+    stack = [end_node]
+    while stack:
+        node = stack.pop()
+        if node in child_counts:
+            child_counts[node] += 1
+        else:
+            child_counts[node] = 1
+            stack.extend(node.parents())
+
+    childless_nodes = [end_node]
+    while childless_nodes:
+        node = childless_nodes.pop()
+        yield node
+        for parent in node.parents():
+            if child_counts[parent] == 1:
+                childless_nodes.append(parent)
+            else:
+                child_counts[parent] -= 1
--- sol_autodiff/tracing.py
+++ sol_autodiff/tracing.py
@@ -32,38 +32,6 @@ class Node:
 
     @classmethod
     def new_root(cls, value):
-        return cls(value, (None, (), {}, []))
+        return cls(value=value, recipe=(None, (), {}, []))
 
 
-def backward(g, end_node):
-    outgrads = {end_node: g}
-    for node in toposort(end_node):
-        outgrad = outgrads.pop(node)
-        fun, args, kwargs, parents = node.recipe
-        for argnum, parent in parents:
-            vjp = fun.get_vjp(argnum)
-            parent_grad = vjp(outgrad, node.value, *args, **kwargs)
-            outgrads[parent] = outgrads.get(parent, 0) + parent_grad
-    return outgrad
-
-
-def toposort(end_node):
-    child_counts = {}
-    stack = [end_node]
-    while stack:
-        node = stack.pop()
-        if node in child_counts:
-            child_counts[node] += 1
-        else:
-            child_counts[node] = 1
-            stack.extend(node.parents())
-
-    childless_nodes = [end_node]
-    while childless_nodes:
-        node = childless_nodes.pop()
-        yield node
-        for parent in node.parents():
-            if child_counts[parent] == 1:
-                childless_nodes.append(parent)
-            else:
-                child_counts[parent] -= 1
